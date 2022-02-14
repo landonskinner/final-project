1b4ec771c66daf381b37207153880db4e7c5a932
Landon Skinner <landonjskinner@gmail.com>
2022-02-14 13:55:00 -0600

    updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://unleashed-frontend.vercel.app'
+    origins '*'
 
     resource '*',
       headers: :any,
